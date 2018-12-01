9a9fa3bf3705d9c57979bbaa3b9c247e318d71cf
richardwiedenroth <rjwiedenroth@crimson.ua.edu>
2018-12-01 13:50:24 -0600

    Added preliminary html/stylesheet stuff to php file.

--- www/search.php
+++ www/search.php
@@ -124,37 +124,101 @@ function bin($results) {
 
 ?>
 
-<!DOCTYPE html>
-<form action="search.php" method="post">
-Search: <input type="text" name="query" />
-<input type="submit" />
-</form>
-<ol>
-<?php
-if($_POST):
-	$results = search($_POST['query']);
-	$maxprice = getMaxPrice($results);
-	echo "Highest price = \$".number_format($maxprice, 2)."<br>";
 
-	$bins = bin($results);
-	$bincrement = $bins->increment;
-	foreach($bins->bins as $bindex=>$bin): ?>
-		<strong>
-			<?php
-			$minPrice = $bindex * $bins->increment;
-			$maxPrice = $minPrice + $bins->increment - 0.01;
-			echo "Bin $bindex (\$".number_format($minPrice, 2)," - \$".number_format($maxPrice,2)."): " . count($bin) . " items" ?>
-		</strong><br>
-		<ol class="bin bin<?=$bindex?>">
-			<?php foreach($bin as $item): ?>
-				<li>
-				<?php if($item->qty > 1): ?>
-					Lot of <?=$item->qty?>:
-				<?php endif; ?>
-					<a href="<?=$item->url?>"><?=$item->title?></a> ($<?=number_format($item->price, 2)?>)
-				</li>
-			<?php endforeach; ?>
-		</ol>
-	<?php endforeach;
-endif;?>
-</ol>
+
+<!DOCTYPE html>
+	<style>
+		body {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			font-family: "fira-sans-2", Verdana, sans-serif;
+		}
+
+		.graph {
+			align-items: flex-end;
+			align-self: center;
+			padding: 0;
+			margin: 0;
+			width: 600px;
+			height: 200px;
+			list-style: none;
+			display: flex;
+			justify-content: space-evenly;
+			flex-wrap: nowrap;
+		}
+
+		.graph-cell {
+			background: #0f7ade;
+			width: 6.25%;
+			margin-left: 2px;
+			margin-right: 2px;
+			transition: .25s;
+		}
+		.graph-cell:hover {
+			background: #2f9cff;
+			transition: .25s;
+		}
+	</style>
+	<ol>
+	<head><meta charset = "utf-8">
+		<title>EbAvg</title>
+		<div style="text-align: center;">
+			<h1>EbAvg</h1>
+			<div class = 'row' >
+				<form action="search.php" method="post">
+					Search: <input type="text" name="query" placeholder="Search..." />
+					<input type="submit" />
+				</form>
+			</div>
+		</div>
+	</head>
+	<body>
+		<ul class="graph">
+			<li class="graph-cell" style="height: 75%;"></li>
+			<li class="graph-cell" style="height: 33%;"></li>
+			<li class="graph-cell" style="height: 60%;"></li>
+			<li class="graph-cell" style="height: 40%;"></li>
+			<li class="graph-cell" style="height: 90%;"></li>
+			<li class="graph-cell" style="height: 10%;"></li>
+			<li class="graph-cell" style="height: 20%;"></li>
+			<li class="graph-cell" style="height: 40%;"></li>
+			<li class="graph-cell" style="height: 90%;"></li>
+			<li class="graph-cell" style="height: 40%;"></li>
+			<li class="graph-cell" style="height: 90%;"></li>
+			<li class="graph-cell" style="height: 10%;"></li>
+			<li class="graph-cell" style="height: 20%;"></li>
+			<li class="graph-cell" style="height: 40%;"></li>
+			<li class="graph-cell" style="height: 90%;"></li>
+			<li class="graph-cell" style="height: 40%;"></li>
+		</ul>
+	</body>
+	<?php
+		if($_POST):
+			$results = search($_POST['query']);
+			$maxprice = getMaxPrice($results);
+			echo "Highest price = \$".number_format($maxprice, 2)."<br>";
+
+			$bins = bin($results);
+			$bincrement = $bins->increment;
+			foreach($bins->bins as $bindex=>$bin): ?>
+				<strong>
+					<?php
+					$minPrice = $bindex * $bins->increment;
+					$maxPrice = $minPrice + $bins->increment - 0.01;
+					echo "Bin $bindex (\$".number_format($minPrice, 2)," - \$".number_format($maxPrice,2)."): " . count($bin) . " items" ?>
+				</strong><br>
+				<ol class="bin bin<?=$bindex?>">
+					<?php foreach($bin as $item): ?>
+						<li>
+						<?php if($item->qty > 1): ?>
+							Lot of <?=$item->qty?>:
+						<?php endif; ?>
+							<a href="<?=$item->url?>"><?=$item->title?></a> ($<?=number_format($item->price, 2)?>)
+						</li>
+					<?php endforeach; ?>
+				</ol>
+			<?php endforeach;
+		endif;?>
+	</ol>
