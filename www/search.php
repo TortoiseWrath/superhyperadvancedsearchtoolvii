7083ca18c4f4daefea0d2caeb15b6f5a12d99667
Sean Gillen <sdgillen@crimson.ua.edu>
2018-12-01 20:46:48 -0600

    fix spinner again

--- www/search.php
+++ www/search.php
@@ -75,51 +75,50 @@ require_once('ebay.php');
 			if(count($results) === 0): ?>
 				<p class="err">No results found.</p>
 			<?php
-				die();
-			endif;
-			$maxprice = getMaxPrice($results);
-			echo '<p class="count">'.count($results).' results';
-			//echo "Highest price = \$".number_format($maxprice, 2)."<br>";
+			else:
+				$maxprice = getMaxPrice($results);
+				echo '<p class="count">'.count($results).' results';
+				//echo "Highest price = \$".number_format($maxprice, 2)."<br>";
 
-			$bins = bin($results);
-			$bincrement = $bins->increment;
-			$maxBinCount = 0;
-			foreach($bins->bins as $bin) {
-				if(count($bin) > $maxBinCount) $maxBinCount = count($bin);
-			}
+				$bins = bin($results);
+				$bincrement = $bins->increment;
+				$maxBinCount = 0;
+				foreach($bins->bins as $bin) {
+					if(count($bin) > $maxBinCount) $maxBinCount = count($bin);
+				}
 
-			?>
-			<ul class="graph">
-				<?php foreach($bins->bins as $bindex=>$bin):
-					$minPrice = $bindex * $bins->increment;
-					$maxPrice = $minPrice + $bins->increment - 0.01; ?>
-					<li class="graph-cell bin<?=$bindex?>" style="height: calc(<?=number_format(count($bin) / $maxBinCount * 100, 2).(count($bin)>0?'% + 2px':'')?>);" title="$<?=number_format($minPrice, 2)." – \$".number_format($maxPrice,2)." (".count($bin)." result".(count($bin)===1?'':'s').")"?>"></li>
-				<?php endforeach; ?>
-			</ul>
-			<div class="xaxis">
-				<div class="min">$0</div>
-				<div class="max">$<?=number_format(ceil($numBins * $bins->increment))?></div>
-				<span>Price</span>
-				<div class="maxbin"><?=$maxBinCount?></div>
-			</div>
-			<?php
-			foreach($bins->bins as $bindex=>$bin): ?>
-				<strong>
-					<?php
-					$minPrice = $bindex * $bins->increment;
-					$maxPrice = $minPrice + $bins->increment - 0.01;
-					echo "<div class=\"bin bin$bindex loading\">\$".number_format($minPrice, 2)." – \$".number_format($maxPrice,2).": " . count($bin) . " results" ?>
-				</strong><br>
-				<ol class="bin bin<?=$bindex?>">
-					<?php foreach($bin as $item): ?>
-						<li<?=$item->qty>1?' class="lot"':''?>>
-							<a href="<?=$item->url?>"><?=$item->title?></a><span class="price"><?=number_format($item->price, 2)?></span><span class="qty"><?=$item->qty?></span><span class="each"><?=number_format($item->price/$item->qty, 2)?></span><span class="eachlabel"> each</span>
-						</li>
+				?>
+				<ul class="graph">
+					<?php foreach($bins->bins as $bindex=>$bin):
+						$minPrice = $bindex * $bins->increment;
+						$maxPrice = $minPrice + $bins->increment - 0.01; ?>
+						<li class="graph-cell bin<?=$bindex?>" style="height: calc(<?=number_format(count($bin) / $maxBinCount * 100, 2).(count($bin)>0?'% + 2px':'')?>);" title="$<?=number_format($minPrice, 2)." – \$".number_format($maxPrice,2)." (".count($bin)." result".(count($bin)===1?'':'s').")"?>"></li>
 					<?php endforeach; ?>
-				</ol></div>
-			<?php endforeach; ?>
-			<div id="dummy">Click a bar in the histogram to view listings in that price bin.</div>
-	<?php endif;?>
+				</ul>
+				<div class="xaxis">
+					<div class="min">$0</div>
+					<div class="max">$<?=number_format(ceil($numBins * $bins->increment))?></div>
+					<span>Price</span>
+					<div class="maxbin"><?=$maxBinCount?></div>
+				</div>
+				<?php
+				foreach($bins->bins as $bindex=>$bin): ?>
+					<strong>
+						<?php
+						$minPrice = $bindex * $bins->increment;
+						$maxPrice = $minPrice + $bins->increment - 0.01;
+						echo "<div class=\"bin bin$bindex loading\">\$".number_format($minPrice, 2)." – \$".number_format($maxPrice,2).": " . count($bin) . " results" ?>
+					</strong><br>
+					<ol class="bin bin<?=$bindex?>">
+						<?php foreach($bin as $item): ?>
+							<li<?=$item->qty>1?' class="lot"':''?>>
+								<a href="<?=$item->url?>"><?=$item->title?></a><span class="price"><?=number_format($item->price, 2)?></span><span class="qty"><?=$item->qty?></span><span class="each"><?=number_format($item->price/$item->qty, 2)?></span><span class="eachlabel"> each</span>
+							</li>
+						<?php endforeach; ?>
+					</ol></div>
+				<?php endforeach; ?>
+				<div id="dummy">Click a bar in the histogram to view listings in that price bin.</div>
+		<?php endif; endif; ?>
 
 <div class="loading" id="loadingDiv">
 	<img src="searchinganim.png" class="loading-image"/>
